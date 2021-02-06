f3fce142ad79d1d1c3885153d72d2739f90dbca6
tokizuoh <k_sakai@yumemi.co.jp>
2021-02-06 11:42:11 +0900

    add: stdout

--- Test20210206/ViewController.swift
+++ Test20210206/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("Hello World!")
+        print("Hello World!")
     }
 
 
